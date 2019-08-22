c7d2d12b3f9584ed85e8f3ca77e8b4e0e71a2958
Travis Holt <travisgholtsr@gmail.com>
2019-08-22 12:48:55 -0700

    Initial Plugin Files Upload

---
+++ includes/class-dashiconic-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://twofour.net
+ * @since      1.0.0
+ *
+ * @package    Dashiconic
+ * @subpackage Dashiconic/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Dashiconic
+ * @subpackage Dashiconic/includes
+ * @author     Two Four <help@twofour.net>
+ */
+class Dashiconic_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-dashiconic-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://twofour.net
+ * @since      1.0.0
+ *
+ * @package    Dashiconic
+ * @subpackage Dashiconic/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Dashiconic
+ * @subpackage Dashiconic/includes
+ * @author     Two Four <help@twofour.net>
+ */
+class Dashiconic_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
